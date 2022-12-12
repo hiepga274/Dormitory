9b14b698823908b8bf6d146bb0ac4084b539734e
hiep274 <hiep1517264@nuce.edu.vn>
2022-12-12 21:12:52 +0700

    hiepdv commit

--- sql.sql
+++ sql.sql
@@ -203,4 +203,72 @@
 			where Amount<Maxx and Room.Id Not In (select RoomId from contract inner join student on contract.studentId = Student.Id where gender <>@Gender)
 			ORDER BY Empty
 		END
+	-- update 12/12/2022 
+	-- Create job update amount of room and status of student when contract time out
+		USE [msdb]
+			GO
+
+			/****** Object:  Job [job_contract_timeout]    Script Date: 12/12/2022 12:21:11 PM ******/
+			BEGIN TRANSACTION
+			DECLARE @ReturnCode INT
+			SELECT @ReturnCode = 0
+			/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 12/12/2022 12:21:11 PM ******/
+			IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+			BEGIN
+			EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+			END
+
+			DECLARE @jobId BINARY(16)
+			EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'job_contract_timeout', 
+					@enabled=1, 
+					@notify_level_eventlog=0, 
+					@notify_level_email=0, 
+					@notify_level_netsend=0, 
+					@notify_level_page=0, 
+					@delete_level=0, 
+					@description=N'No description available.', 
+					@category_name=N'[Uncategorized (Local)]', 
+					@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			/****** Object:  Step [contract_timeout]    Script Date: 12/12/2022 12:21:11 PM ******/
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'contract_timeout', 
+					@step_id=1, 
+					@cmdexec_success_code=0, 
+					@on_success_action=1, 
+					@on_success_step_id=0, 
+					@on_fail_action=2, 
+					@on_fail_step_id=0, 
+					@retry_attempts=0, 
+					@retry_interval=0, 
+					@os_run_priority=0, @subsystem=N'TSQL', 
+					@command=N'exec contract_timeout', 
+					@database_name=N'DormitoryDB', 
+					@flags=0
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'CollectorSchedule_Every_15min', 
+					@enabled=1, 
+					@freq_type=4, 
+					@freq_interval=1, 
+					@freq_subday_type=4, 
+					@freq_subday_interval=15, 
+					@freq_relative_interval=0, 
+					@freq_recurrence_factor=0, 
+					@active_start_date=20190924, 
+					@active_end_date=99991231, 
+					@active_start_time=0, 
+					@active_end_time=235959, 
+					@schedule_uid=N'b4829dad-5f2b-4e14-94b0-ccba6ba75bea'
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			COMMIT TRANSACTION
+			GOTO EndSave
+			QuitWithRollback:
+				IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+			EndSave:
+			GO
         
